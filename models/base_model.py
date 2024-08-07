997f18cd4a3400a0476be4c2b743d3a40a588ef7
Wairegi Whitney <wairegiwhitney11@gmail.com>
2024-08-07 15:56:08 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,23 @@
-print("herery")
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, id, name,updated_at=None,created_at=None):
+        self.id= str(uuid.uuid4())
+        self.name= name
+        self.updated_at=datetime.now()
+        self.created_at=datetime.now()
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,self.id,self.__dict__)
+    def save(self):
+        self.updated_at=datetime.now()
+    def to_dict(self):
+        my_dict=self.__dict__.copy()
+        my_dict["__class__"]=self.__class__.__name__
+        my_dict["created_at"]=my_dict["created_at"].isoformat()
+        my_dict["updated_at"]=my_dict["updated_at"].isoformat()
+        
+
+        return my_dict
+
+  
