33cf5d66d0fe62ef145f0b9e8f888d5ef0261674
Wairegi Whitney <wairegiwhitney11@gmail.com>
2024-08-07 16:54:45 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,21 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, id, name,updated_at=None,created_at=None):
-        self.id= str(uuid.uuid4())
-        self.name= name
-        self.updated_at=datetime.now()
-        self.created_at=datetime.now()
+    def __init__(self,*args, **kwargs):
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,self.id,self.__dict__)
     def save(self):
